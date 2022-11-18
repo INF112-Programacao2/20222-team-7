3b181b7935c988ae406492bfbfb77fe540cc5c57
anaclaraguerra <113150075+anaclaraguerra@users.noreply.github.com>
2022-11-18 14:44:11 -0300

    tirei gorjeta, coloquei no "pedidos"

--- Item/Item.h
+++ Item/Item.h
@@ -2,7 +2,7 @@
 #define ITEM_H
 
 #include <string>
-#include "Garcom.h"
+
 
 class Item
 {
@@ -20,7 +20,7 @@ class Item
         double get_preco();
         double get_tempo_preparo();
         int get_codigo();
-        void passargorjeta();            //10% do preco
+
 };
 
 #endif
