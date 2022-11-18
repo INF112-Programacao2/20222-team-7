350c24ac75a9cb87bdcd5a69f87edeabcb547451
anaclaraguerra <113150075+anaclaraguerra@users.noreply.github.com>
2022-11-18 14:20:22 -0300

    construtor, polimorfismo calcular salario com gorjeta

--- Garcom/Garcom.cpp
+++ Garcom/Garcom.cpp
@@ -2,18 +2,34 @@
 #include "Garcom.h"
 #include <iostream>
 
+Garcom::Garcom(std::string nome, int codigo, std::string contratadoem, int cargahoraria, int avaliacao, std::string telefone, double gorjetatotaldomes){
+    _nome = nome;
+    _codigo = codigo;
+    _data_contrato = contratadoem;
+    _carga_horaria = cargahoraria;
+    _avaliacao = avaliacao;
+    _telefone = telefone;
+    _salariominimo = 1200.00
+    _horaextra = 0;
+    _gorjetatotaldomes = gorjetatotaldomes;
+
+}
+
+void Funcionario::calcular_salariofinal(){
+    if(_carga_horaria <= 8){
+        _salariofinal = _salariominimo + _gorjetasdodia;
+    } else{
+        _horaextra = 8 - _carga_horaria;
+        _salariofinal = _salariominimo + 5.5 * (_horaextra) + _gorjetatotaldomes;         
+    }
+
+}
+
 void Garcom::verPedidos(){
 
-<<<<<<< HEAD
 }
 
 void Garcom::finalizarPedidos(){
-
 }
-=======
-};
 
-void Garcom::finalizarPedidos(){
 
-};
->>>>>>> 9d5f263f12945717fdfa0b6babac866bb4308fcf
